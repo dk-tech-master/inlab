50ff985097af148f7d5f75a5d84d39fca5b61883
cheoljin408 <kcj409@naver.com>
2023-06-27 17:46:20 +0900

    api first commit

---
+++ inlab-api/src/main/java/kr/inlab/www/InlabApiApplication.java
@@ -0,0 +1,16 @@
+package kr.inlab.www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
+
+@SpringBootApplication
+public class InlabApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication application = new SpringApplication(InlabApiApplication.class);
+        application.addListeners(new ApplicationPidFileWriter());
+        application.run(args);
+    }
+
+}
---
+++ inlab-api/src/test/java/kr/inlab/www/InlabApiApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.inlab.www;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InlabApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
