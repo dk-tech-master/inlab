2d5fa7bfe426c5455e5e178436c863ddab0c7183
choijungwoo493 <jwoo1016@gmail.com>
2023-06-29 12:01:50 +0900

    feat: User 엔티티 수정
    - entity validation 추가
    - JPA auditing 적용
    - columnDefinition 로 Default 설정
    - UserStatus String -> enum 으로 관리
    - builder 생성자 추가
    - DynamicInsert 로 null 을 제외한 컬럼만 insert
    - 기본생성자의 접근 범위를 Protected 로 수정

--- inlab-api/src/main/java/kr/inlab/www/entity/User.java
+++ inlab-api/src/main/java/kr/inlab/www/entity/User.java
@@ -5,46 +5,73 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import kr.inlab.www.common.type.UserStatus;
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
+import org.hibernate.annotations.DynamicInsert;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
 @Getter
-@Setter
-@Table(name = "user")
-@NoArgsConstructor
+@DynamicInsert
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userId;
 
+    @NotNull
     @Column(nullable = false, length = 50, unique = true)
     private String email;
+
+    @NotNull
     @Column(nullable = false, length = 50, unique = true)
     private String nickname;
 
+    @NotNull
     @Column(nullable = false, length = 500)
     private String password;
 
+    @CreatedDate
     private LocalDateTime passwordModifiedAt;
 
+    @Column(columnDefinition = "varchar(1) default 'N'")
     private Boolean verificationStatus;
 
+    @Column(columnDefinition = "tinyint default 0")
     private Integer loginAttempt;
 
-    private String userStatus;
+    @Column(columnDefinition = "varchar(255) default 'ACTIVE'")
+    @Enumerated(value = EnumType.STRING)
+    private UserStatus userStatus;
 
+    @CreatedDate
     private LocalDateTime createdAt;
 
+    @LastModifiedDate
     private LocalDateTime modifiedAt;
 
     @OneToMany(mappedBy = "user")
     private List<Role> roles = new ArrayList<>();
+
+    @Builder
+    public User(String email, String nickname, String password) {
+        this.email = email;
+        this.nickname = nickname;
+        this.password = password;
+    }
 }
