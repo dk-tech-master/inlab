371ac599a4bb60f250c41292e4e1c3460e1eb37d
Shin Dain <codedain@gmail.com>
2023-07-01 19:18:33 +0900

    feat: 비밀번호변경페이지 ui 구현

--- inlab-ui/tailwind.config.js
+++ inlab-ui/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // primary: "#4f46e5",
+      },
+    },
   },
   plugins: [require("daisyui")],
 };
