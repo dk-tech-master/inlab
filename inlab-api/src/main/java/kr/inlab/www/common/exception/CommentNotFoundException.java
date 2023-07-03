2687fbde04a3bc09304fb4954feab5014ab1c0f3
cheoljin408 <kcj409@naver.com>
2023-07-03 16:13:51 +0900

    feat: status code 수정

--- inlab-api/src/main/java/kr/inlab/www/common/exception/CommentNotFoundException.java
+++ inlab-api/src/main/java/kr/inlab/www/common/exception/CommentNotFoundException.java
@@ -1,5 +1,7 @@
 package kr.inlab.www.common.exception;
 
+import org.springframework.http.HttpStatus;
+
 public class CommentNotFoundException extends InlabException {
 
     private static final String MESSAGE = "존재하지 않는 코멘트입니다.";
@@ -10,6 +12,6 @@ public class CommentNotFoundException extends InlabException {
 
     @Override
     public int getStatusCode() {
-        return 404;
+        return HttpStatus.NOT_FOUND.value();
     }
 }
