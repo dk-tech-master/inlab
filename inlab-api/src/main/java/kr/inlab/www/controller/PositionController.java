50bb5ab5c6851ebdce0625459e207b7af6d752e9
juhee <cbjh05@gmail.com>
2023-07-03 10:13:47 +0900

    feat: 직무 조회 api 구현 - 재 업로드
    - pageUtil 추가
    - 직무 조회 api TEST 코드 추가

--- inlab-api/src/main/java/kr/inlab/www/controller/PositionController.java
+++ inlab-api/src/main/java/kr/inlab/www/controller/PositionController.java
@@ -1,6 +1,8 @@
 package kr.inlab.www.controller;
 
 import kr.inlab.www.dto.request.RequestPositionDto;
+import kr.inlab.www.dto.request.RequestPositionNameDto;
+import kr.inlab.www.dto.response.ResponsePositionListDto;
 import kr.inlab.www.repository.PositionRepository;
 import kr.inlab.www.service.PositionService;
 import lombok.RequiredArgsConstructor;
@@ -15,10 +17,16 @@ public class PositionController {
 
     private final PositionService positionService;
     @PostMapping
-    public ResponseEntity<?> createPosition(@RequestBody RequestPositionDto dto){
+    public ResponseEntity<?> createPosition(@RequestBody RequestPositionNameDto dto){
         positionService.createPosition(dto.getPositionName());
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .build();
+    }
+
+    @GetMapping
+    public ResponseEntity<ResponsePositionListDto> getPosition(RequestPositionDto requestDto) {
+        ResponsePositionListDto responseDto = positionService.getPosition(requestDto);
+        return ResponseEntity.ok(responseDto);
     }
 }
