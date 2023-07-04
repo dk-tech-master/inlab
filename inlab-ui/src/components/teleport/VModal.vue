9c64bce59c1be68ce525fe937486143b26a37299
Shin Dain <codedain@gmail.com>
2023-07-04 11:27:54 +0900

    feat: 사용자 관리 모달창 ui 구현

---
+++ inlab-ui/src/components/teleport/VModal.vue
@@ -0,0 +1,12 @@
+<template>
+  <dialog id="my_modal_5" class="modal modal-bottom sm:modal-middle">
+    <form method="dialog" class="modal-box pt-12 py-8">
+      <slot name="header"></slot>
+      <slot name="body"></slot>
+      <slot name="footer"></slot>
+    </form>
+  </dialog>
+</template>
+<script setup></script>
+
+<style scoped></style>
