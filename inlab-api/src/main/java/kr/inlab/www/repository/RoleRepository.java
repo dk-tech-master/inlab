5ec085139718d9b3a4a8a4db2aa6c5de832b5362
choijungwoo493 <jwoo1016@gmail.com>
2023-07-05 11:53:34 +0900

    feat: 관리자가 회원가입 승인 시 role 을 ROLE_USER 로 변경

--- inlab-api/src/main/java/kr/inlab/www/repository/RoleRepository.java
+++ inlab-api/src/main/java/kr/inlab/www/repository/RoleRepository.java
@@ -1,6 +1,7 @@
 package kr.inlab.www.repository;
 
 import java.util.List;
+import kr.inlab.www.common.type.RoleType;
 import kr.inlab.www.entity.Role;
 import kr.inlab.www.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,4 +9,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface RoleRepository extends JpaRepository<Role, Integer> {
 
     List<Role> findByUser(User user);
+
+    Role findByUserAndRoleType(User user, RoleType roleGuest);
 }
