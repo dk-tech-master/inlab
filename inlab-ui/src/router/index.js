936d7e6215a06aadc4a233d7458483b7f5f8d604
Shin Dain <codedain@gmail.com>
2023-07-05 15:10:43 +0900

    feat: 공통 컴포넌트 ui
    1. Header
    2. SearchFilter - 검색 필터
    3. QuestionManagementForm - 질문 등록과 수정에 쓰일 공통 form
    4. Pagination

--- inlab-ui/src/router/index.js
+++ inlab-ui/src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import HomeView from "../views/TestView.vue";
 import UserLogin from "../views/users/UserLogin.vue";
 import SignUp from "../views/users/SignUp.vue";
 import UpdatePassword from "../views/users/UpdatePassword.vue";
