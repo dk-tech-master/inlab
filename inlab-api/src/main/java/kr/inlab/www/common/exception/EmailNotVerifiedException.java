81b2ffc52170e25879e589d1fea0545293df574f
choijungwoo493 <jwoo1016@gmail.com>
2023-07-05 15:40:31 +0900

    feat: Throwable 상속받던 exception 을 Runtime 을 상속받게 변경

--- inlab-api/src/main/java/kr/inlab/www/common/exception/EmailNotVerifiedException.java
+++ inlab-api/src/main/java/kr/inlab/www/common/exception/EmailNotVerifiedException.java
@@ -1,6 +1,6 @@
 package kr.inlab.www.common.exception;
 
-public class EmailNotVerifiedException extends Throwable {
+public class EmailNotVerifiedException extends RuntimeException {
     public static final String MESSAGE = "이메일 인증을 진행해주세요.";
 
     public EmailNotVerifiedException() {
--- inlab-api/src/main/java/kr/inlab/www/common/exception/ExpiredVerificationCodeException.java
+++ inlab-api/src/main/java/kr/inlab/www/common/exception/ExpiredVerificationCodeException.java
@@ -1,6 +1,6 @@
 package kr.inlab.www.common.exception;
 
-public class ExpiredVerificationCodeException extends Throwable {
+public class ExpiredVerificationCodeException extends RuntimeException {
     public static final String MESSAGE = "인증 시간이 만료되었습니다. 다시 인증을 진행해주세요.";
 
     public ExpiredVerificationCodeException() {
