5edf42316cb6e7ddbdbad536f6f7b60a63378d0c
choijungwoo493 <jwoo1016@gmail.com>
2023-07-05 15:38:53 +0900

    feat: 인증번호 확인 메서드 테스트 작성

--- inlab-api/src/test/java/kr/inlab/www/controller/VerificationCodeControllerTest.java
+++ inlab-api/src/test/java/kr/inlab/www/controller/VerificationCodeControllerTest.java
@@ -1,11 +1,16 @@
 package kr.inlab.www.controller;
 
+import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
+import static org.springframework.restdocs.headers.HeaderDocumentation.responseHeaders;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import kr.inlab.www.dto.request.RequestCheckVerificationCode;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -73,4 +78,31 @@ class VerificationCodeControllerTest {
         );
     }
 
+    @Test
+    @Transactional
+    void checkVerificationCode_shouldAddTokenToResponseHeader() throws Exception {
+        // Arrange
+        String email = "jwoo1016@naver.com";
+        String verificationCode = "C9E8807B-5192-4C5A-81B3-15D92D15B1C0";
+
+        RequestCheckVerificationCode requestDto = new RequestCheckVerificationCode(email,verificationCode);
+
+        // Act
+        ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.post("/api/verification_code/check")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(requestDto)))
+            .andExpect(status().isOk());
+
+        // Assert
+        resultActions.andDo(document("check-verification-code",
+            requestFields(
+                fieldWithPath("email").description("이메일"),
+                fieldWithPath("verificationCode").description("12314")
+            ),
+            responseHeaders(
+                headerWithName("email").description("이메일 인증을 하면 발행되는 이메일 정보가 들어있는 jwt 토큰")
+                // 필요한 만큼 토큰 정보를 추가하세요
+            )
+        ));
+    }
 }
