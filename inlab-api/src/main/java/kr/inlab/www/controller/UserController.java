295c2aed929a1004a0f0b1b9a1dd4354a2aaf2c3
choijungwoo493 <jwoo1016@gmail.com>
2023-07-05 17:26:25 +0900

    feat: controller 에 주석 추가

--- inlab-api/src/main/java/kr/inlab/www/controller/UserController.java
+++ inlab-api/src/main/java/kr/inlab/www/controller/UserController.java
@@ -8,8 +8,6 @@ import kr.inlab.www.dto.common.ResponseListDto;
 import kr.inlab.www.dto.request.RequestCreateUserDto;
 import kr.inlab.www.dto.request.RequestUpdateUserDto;
 import kr.inlab.www.dto.request.RequestUsersDto;
-import kr.inlab.www.dto.response.ResponseGetUserDto;
-import kr.inlab.www.entity.User;
 import kr.inlab.www.security.jwt.JwtTokenProvider;
 import kr.inlab.www.service.UserService;
 import lombok.RequiredArgsConstructor;
@@ -53,7 +51,10 @@ public class UserController {
     }
 
     /**
-     * 헤더에 클리어 해야할 토큰 정보를 추가합니다.
+     * @param request 인증 버튼을 누르면 받는 이메일 정보가 들어있는 jwt 토큰이 있습니다.
+     * @param dto     회원 가입 양식에 적힌 정보입니다. 이메일, 닉네임, 비밀번호가 있습니다.
+     * @throws EmailNotVerifiedException
+     * @throws EmailDuplicateException   특이 사항: 헤더에 클리어 해야할 토큰 정보(email 토큰)를 추가합니다.
      */
     @PostMapping("/users")
     public ResponseEntity createUser(HttpServletRequest request, @RequestBody RequestCreateUserDto dto)
@@ -63,12 +64,24 @@ public class UserController {
         return new ResponseEntity(headers, HttpStatus.CREATED);
     }
 
+    /**
+     * 관리자가 면접관들을 다건 조회하는 메서드
+     *
+     * @param requestDto 검색조건(닉네임, 인증여부) 가 들어있습니다.
+     * @return 검색 결과
+     */
     @GetMapping("/admin/users")
     public ResponseEntity<ResponseListDto> getUsers(@RequestBody RequestUsersDto requestDto) {
-        ResponseListDto<ResponseGetUserDto> users = userService.getUsers(requestDto);
-        return ResponseEntity.status(HttpStatus.OK).body(users);
+        return ResponseEntity.status(HttpStatus.OK).body(userService.getUsers(requestDto));
     }
 
+    /**
+     * 사용자가 자신의 정보를 수정하는 메서드
+     *
+     * @param request 이메일 인증을 하면 발급되는 email 토큰이 들어있습니다.
+     * @param dto     수정할 회원정보가 들어있습니다.
+     * @return
+     */
     @PutMapping("/users/{userId}")
     public ResponseEntity updateUser(HttpServletRequest request, @RequestBody RequestUpdateUserDto dto)
         throws EmailNotVerifiedException {
@@ -77,18 +90,36 @@ public class UserController {
         return new ResponseEntity(headers, HttpStatus.OK);
     }
 
+    /**
+     * 관리자가 회원의 권한을 ROLE_GUEST -> ROLE_USER 로 바꾸는 메서드입니다.
+     * @param userId
+     * @return
+     */
     @PutMapping("/admin/users/{userId}/role")
     public ResponseEntity updateUserRoleGuestToUser(@PathVariable Long userId) {
         userService.updateUserRoleGuestToUser(userId);
         return new ResponseEntity(HttpStatus.OK);
     }
 
+    /**
+     * 관리자가 회원의 상태를 delete 로 바꾸는 메서드입니다.
+     *
+     * @param userId delete 될 회원의 userId
+     * @return
+     */
     @DeleteMapping("/admin/users/{userId}")
     public ResponseEntity updateUserStatusDeleteByAdmin(@PathVariable Long userId) {
         userService.updateUserStatusDelete(userId);
         return new ResponseEntity(HttpStatus.OK);
     }
 
+    /**
+     * 회원이 자신의 상태를 delete 로 바꾸는 메서드입니다.
+     *
+     * @param username 회원의 email
+     * @param userId   회원의 userId
+     * @return
+     */
     @DeleteMapping("/users/{userId}")
     public ResponseEntity updateUserStatusDelete(@AuthenticationPrincipal String username, @PathVariable Long userId) {
         userService.updateUserStatusDelete(username, userId);
