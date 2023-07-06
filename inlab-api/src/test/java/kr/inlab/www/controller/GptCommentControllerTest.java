9ffc7adad516bcb68a242a2101f23fc132e082e3
cheoljin408 <kcj409@naver.com>
2023-07-06 16:20:41 +0900

    test: ChatGPT 평가요청 API 테스트 코드 작성

---
+++ inlab-api/src/test/java/kr/inlab/www/controller/GptCommentControllerTest.java
@@ -0,0 +1,64 @@
+package kr.inlab.www.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
+import org.springframework.test.web.servlet.MockMvc;
+
+import javax.transaction.Transactional;
+
+import java.nio.charset.StandardCharsets;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
+import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc(addFilters = false)
+@AutoConfigureRestDocs
+@ExtendWith(RestDocumentationExtension.class)
+class GptCommentControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Transactional
+    @Test
+    void 지피티_평가요청결과_조회() throws Exception {
+        mockMvc.perform(get("/api/gpt-comment/{gptCommentId}", 21L)
+                        .header(HttpHeaders.AUTHORIZATION, "jwt token")
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andDo(document(
+                        "get-gpt-comment",
+                        pathParameters(parameterWithName("gptCommentId").description("GPT 코멘트 ID")),
+                        requestHeaders(headerWithName("Authorization").description("JWT Token")),
+                        responseFields(
+                                fieldWithPath("gptCommentId").description("GPT 코멘트 ID"),
+                                fieldWithPath("requestContent").description("GPT에게 요청한 내용"),
+                                fieldWithPath("responseContent").description("GPT에게 응답받은 내용")
+                        )
+                ));
+    }
+}
