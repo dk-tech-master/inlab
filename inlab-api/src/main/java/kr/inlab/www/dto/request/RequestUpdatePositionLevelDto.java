3b59246fcedb470a463cf1cc58fbc8c3fac35871
choijungwoo493 <jwoo1016@gmail.com>
2023-07-07 17:31:35 +0900

    feat: PositionLevelController restDocs 작성

--- inlab-api/src/main/java/kr/inlab/www/dto/request/RequestUpdatePositionLevelDto.java
+++ inlab-api/src/main/java/kr/inlab/www/dto/request/RequestUpdatePositionLevelDto.java
@@ -2,6 +2,7 @@ package kr.inlab.www.dto.request;
 
 import java.util.List;
 import kr.inlab.www.dto.common.PositionAndLevelInfo;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
@@ -10,4 +11,11 @@ public class RequestUpdatePositionLevelDto {
     private Long userId;
 
     private List<PositionAndLevelInfo> positionAndLevelInfos;
+
+    @Builder
+    public RequestUpdatePositionLevelDto(Long userId,
+        List<PositionAndLevelInfo> positionAndLevelInfos) {
+        this.userId = userId;
+        this.positionAndLevelInfos = positionAndLevelInfos;
+    }
 }
