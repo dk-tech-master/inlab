a09cb6bf11500763ca17575a10838b77d28b30f5
cheoljin408 <kcj409@naver.com>
2023-07-12 20:04:17 +0900

    test: 면접결과(코멘트) 수정 API 테스트 코드 작성 및 REST Docs 추가

--- inlab-api/src/test/java/kr/inlab/www/controller/CommentControllerTest.java
+++ inlab-api/src/test/java/kr/inlab/www/controller/CommentControllerTest.java
@@ -1,6 +1,7 @@
 package kr.inlab.www.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import kr.inlab.www.dto.request.RequestUpdateCommentDto;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,18 +11,18 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.request.RequestDocumentation;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.nio.charset.StandardCharsets;
+
 import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
 import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
-import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
-import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
-import static org.springframework.restdocs.request.RequestDocumentation.*;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.patch;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -38,11 +39,32 @@ class CommentControllerTest {
     private ObjectMapper objectMapper;
 
     @Test
-    void 코멘트_조회() throws Exception {
-        mockMvc.perform(get("/api/comment/{commentId}", "0")
+    void 코멘트_수정() throws Exception {
+        RequestUpdateCommentDto requestDto = RequestUpdateCommentDto.builder()
+                .content("좋은 답변입니다.")
+                .build();
+        String json = objectMapper.writeValueAsString(requestDto);
+        mockMvc.perform(patch("/api/comment/{commentId}", 1L)
                         .header(HttpHeaders.AUTHORIZATION, "jwt token")
-                        .accept(MediaType.APPLICATION_JSON))
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
                 .andDo(print())
-                .andExpect(status().isNotFound());
+                .andExpect(status().isOk())
+                .andDo(document(
+                                "update-comment",
+                                pathParameters(parameterWithName("commentId").description("코멘트 ID")),
+                                requestHeaders(headerWithName("Authorization").description("JWT Token")),
+                                requestFields(
+                                        fieldWithPath("content").description("수정될 코멘트 내용")
+                                ),
+                                responseFields(
+                                        fieldWithPath("commentId").description("코멘트 ID"),
+                                        fieldWithPath("content").description("코멘트 내용"),
+                                        fieldWithPath("createdAt").description("생성일자"),
+                                        fieldWithPath("modifiedAt").description("수정일자")
+                                )
+                        )
+                );
     }
 }
