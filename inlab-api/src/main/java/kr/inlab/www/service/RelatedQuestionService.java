77121a2fa05b4de4c3016b92f74f5b37d9c9cf93
LEEGWONSEOK <leestart1023@gmail.com>
2023-07-18 17:28:25 +0900

    feat: 꼬리질문 중복 select 없애기, 질문 리스트 ADMIN 권한 추가
    - 꼬리질문 등록 시 중복 select 없애기
    - 질문 리스트에도 ADMIN 권한 추가
    - 꼬리질문 조회 시 relatedQuestionId 값 응답에 추가

--- inlab-api/src/main/java/kr/inlab/www/service/RelatedQuestionService.java
+++ inlab-api/src/main/java/kr/inlab/www/service/RelatedQuestionService.java
@@ -4,11 +4,12 @@ import kr.inlab.www.dto.common.ResponseListDto;
 import kr.inlab.www.dto.request.RequestCreateRelatedQuestionDto;
 import kr.inlab.www.dto.request.RequestRelatedQuestionsDto;
 import kr.inlab.www.dto.response.ResponseGetQuestionsDto;
+import kr.inlab.www.dto.response.ResponseGetRelatedQuestionsDto;
 
 public interface RelatedQuestionService {
 	void createRelatedQuestion(RequestCreateRelatedQuestionDto requestDto);
 
 	void deleteRelatedQuestion(Long relatedQuestionId);
 
-	ResponseListDto<ResponseGetQuestionsDto> getRelatedQuestions(RequestRelatedQuestionsDto requestDto);
+	ResponseListDto<ResponseGetRelatedQuestionsDto> getRelatedQuestions(RequestRelatedQuestionsDto requestDto);
 }
--- inlab-api/src/main/java/kr/inlab/www/service/RelatedQuestionServiceImpl.java
+++ inlab-api/src/main/java/kr/inlab/www/service/RelatedQuestionServiceImpl.java
@@ -15,7 +15,7 @@ import kr.inlab.www.common.util.PagingUtil;
 import kr.inlab.www.dto.common.ResponseListDto;
 import kr.inlab.www.dto.request.RequestCreateRelatedQuestionDto;
 import kr.inlab.www.dto.request.RequestRelatedQuestionsDto;
-import kr.inlab.www.dto.response.ResponseGetQuestionsDto;
+import kr.inlab.www.dto.response.ResponseGetRelatedQuestionsDto;
 import kr.inlab.www.entity.Question;
 import kr.inlab.www.entity.RelatedQuestion;
 import kr.inlab.www.repository.QuestionRepository;
@@ -37,6 +37,10 @@ public class RelatedQuestionServiceImpl implements RelatedQuestionService {
 		Question tailQuestion = questionRepository.findById(requestDto.getTailQuestionId())
 			.orElseThrow(QuestionNotFoundException::new);
 
+		if (relatedQuestionRepository.existsByHeadQuestionAndTailQuestion(headQuestion, tailQuestion)) {
+			throw new RuntimeException("이미 있는 꼬리질문입니다");
+		}
+
 		relatedQuestionRepository.save(RelatedQuestion.builder()
 			.headQuestion(headQuestion)
 			.tailQuestion(tailQuestion)
@@ -45,18 +49,20 @@ public class RelatedQuestionServiceImpl implements RelatedQuestionService {
 
 	@Transactional
 	@Override
-	public ResponseListDto<ResponseGetQuestionsDto> getRelatedQuestions(RequestRelatedQuestionsDto requestDto) {
-		Pageable pageable = PageRequest.of(requestDto.getPage(), requestDto.getPageSize(), Sort.by(Sort.Direction.DESC, "relatedQuestionId"));
+	public ResponseListDto<ResponseGetRelatedQuestionsDto> getRelatedQuestions(RequestRelatedQuestionsDto requestDto) {
+		Pageable pageable = PageRequest.of(requestDto.getPage(), requestDto.getPageSize(),
+			Sort.by(Sort.Direction.DESC, "relatedQuestionId"));
 		Page<RelatedQuestion> questionList = relatedQuestionRepository.findAllByHeadQuestionQuestionId(
 			requestDto.getQuestionId(),
 			pageable
 		);
 
-		List<ResponseGetQuestionsDto> dtoList = questionList.stream()
-			.map(RelatedQuestion::toResponseQuestionsDto).collect(Collectors.toList());
-		PagingUtil pagingUtil = new PagingUtil(questionList.getTotalElements(), questionList.getTotalPages(), questionList.getNumber(), questionList.getSize());
+		List<ResponseGetRelatedQuestionsDto> dtoList = questionList.stream()
+			.map(RelatedQuestion::toResponseRelatedQuestionsDto).collect(Collectors.toList());
+		PagingUtil pagingUtil = new PagingUtil(questionList.getTotalElements(), questionList.getTotalPages(),
+			questionList.getNumber(), questionList.getSize());
 
-		return ResponseListDto.<ResponseGetQuestionsDto>builder()
+		return ResponseListDto.<ResponseGetRelatedQuestionsDto>builder()
 			.responseList(dtoList)
 			.pagingUtil(pagingUtil)
 			.build();
