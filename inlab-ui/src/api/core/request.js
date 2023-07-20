d31eca21313b54395252044bbbb89eefb18ab8a7
choijungwoo493 <jwoo1016@gmail.com>
2023-07-20 14:57:24 +0900

    feat: 관리자 계정 삭제 버튼 클릭 시 예외 UI 구현

--- inlab-ui/src/api/core/request.js
+++ inlab-ui/src/api/core/request.js
@@ -52,8 +52,7 @@ request.interceptors.response.use(
     return response;
   },
   async (error) => {
-    console.error(error);
-    // alert(error.response.data.message);
+    alert(error.response.data.message);
     return Promise.reject(error);
   },
 );
