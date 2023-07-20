8b61adbc68311458a2c53b54450f4fa83fd4e194
choijungwoo493 <jwoo1016@gmail.com>
2023-07-20 10:46:18 +0900

    feat: 권한에 따른 사이드바 UI 수정

--- inlab-ui/src/components/common/SideBar.vue
+++ inlab-ui/src/components/common/SideBar.vue
@@ -13,7 +13,7 @@
           <p class="mt-3 ml-1 text-sm text-gray-500">면접 관리 시스템</p>
         </li>
         <!--        1. 면접관 관리-->
-        <li>
+        <li v-if="role === 'ROLE_ADMIN'">
           <a
             href="/interviewer"
             class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-200 hover:text-primary"
@@ -36,32 +36,8 @@
             <span class="ml-3">면접관 관리</span>
           </a>
         </li>
-        <!--        2. 질문관리-->
-        <li>
-          <a
-            href="/question"
-            class="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-200 hover:text-primary"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              class="w-6 h-6"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25zM6.75 12h.008v.008H6.75V12zm0 3h.008v.008H6.75V15zm0 3h.008v.008H6.75V18z"
-              />
-            </svg>
-
-            <span class="ml-3">질문 관리</span>
-          </a>
-        </li>
-        <!--        3. 질문 카테고리 관리-->
-        <li>
+        <!--        2. 질문 카테고리 관리-->
+        <li v-if="role === 'ROLE_ADMIN'">
           <button
             @click="toggleQuestionCategory(1)"
             type="button"
@@ -126,6 +102,30 @@
               >
             </li>
           </ul>
+        </li>
+        <!--        3. 질문관리-->
+        <li>
+          <a
+            href="/question"
+            class="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-200 hover:text-primary"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-6 h-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25zM6.75 12h.008v.008H6.75V12zm0 3h.008v.008H6.75V15zm0 3h.008v.008H6.75V18z"
+              />
+            </svg>
+
+            <span class="ml-3">질문 관리</span>
+          </a>
         </li>
         <!--        4. 면접관리-->
         <li>
@@ -196,6 +196,7 @@ import { ref } from "vue";
 
 const questionCategoryOpen = ref(false);
 const interviewOpen = ref(false);
+const role = sessionStorage.getItem("role");
 
 const toggleQuestionCategory = (index) => {
   if (index === 1) {
