a8efbab9030bd02608f58ab7fc98484cdcb1b5e7
choijungwoo493 <jwoo1016@gmail.com>
2023-07-21 12:30:43 +0900

    feat: Runtime 에러 -> InlabException 변경 적용

--- inlab-api/src/main/java/kr/inlab/www/common/exception/EmailAlreadyExistsException.java
+++ inlab-api/src/main/java/kr/inlab/www/common/exception/EmailAlreadyExistsException.java
@@ -1,9 +1,16 @@
 package kr.inlab.www.common.exception;
 
-public class EmailAlreadyExistsException extends RuntimeException {
+import org.springframework.http.HttpStatus;
+
+public class EmailAlreadyExistsException extends InlabException {
     public static final String MESSAGE = "이미 사용중인 이메일입니다.";
 
     public EmailAlreadyExistsException() {
         super(MESSAGE);
+    }
+
+    @Override
+    public int getStatusCode() {
+        return HttpStatus.NOT_FOUND.value();
     }
 }
--- inlab-api/src/main/java/kr/inlab/www/common/exception/UserNotFoundException.java
+++ inlab-api/src/main/java/kr/inlab/www/common/exception/UserNotFoundException.java
@@ -2,7 +2,6 @@ package kr.inlab.www.common.exception;
 
 import org.springframework.http.HttpStatus;
 
-//todo Runtime 과 Throwable 중 뭘 던지지?
 public class UserNotFoundException extends InlabException {
 
     public static final String MESSAGE = "사용자 정보를 찾을 수 없습니다.";
